ff754ff7b386b98a29e6bced334c3d8d098d92c8
Duong Dang <danghuuduong98@gmail.com>
2023-09-18 17:29:06 +0700

    add package

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const http = require('http')
 
-const hostname = '127.0.0.1'
-const port = 3001
+const express = require('express')
+const app = express()
+const port = 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World\n asdasd ')
+app.get('/', (req, res) => {
+  res.send('Hello World!aa', req, res)
 })
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
